eb886d2c72fba047839ce167da31c018e5a1c0a8
Lee Clarke <lee.k.clarke@gmail.com>
2011-04-25 23:18:29 -0400

    added fm to index, needs formattion

---
+++ js/rest_manager.js
@@ -0,0 +1,45 @@
+//Manages the rest requests for various feeds. Uses JSONP to do off site calls.
+
+var feedItems = [];
+var target;
+
+/**
+ * Makes a get JSON call with the callback indicator that forces the JsonP workaround.  
+ */
+function retrieveRestFeed(feedUrl, resultTarget) {
+	target = resultTarget;
+	$.getJSON(feedUrl, parseFM);
+	
+}
+
+/**
+ * Parses and sets the lastFL data. 
+ */
+function parseFM(data){
+	if(data.entries) {		
+		
+		console.log("entry count="+data.entries.length + "   resultTarget="+target)	
+		for(i = 0; i<data.entries.length; i++) {
+			var entry = data.entries[i];
+			var title = entry.title.value;
+			var date = entry.property_map.modified;
+			console.log('title='+title);
+			feedItems.push('<li><span id="title">' + title +'</div><div id="listen-date">' + parseToLocal(date)+ '</div></li>');
+		}
+	}
+	var out = feedItems.join()
+	while(out.indexOf(',') >-1) {
+		out = out.replace(',',' ');
+	}
+	$(target).html(out);
+}
+
+
+function parseToLocal(dateStr) {
+	var resp = "Not Currently Available";
+		if(dateStr) {
+			var d = new Date(Date.parse(dateStr));
+			resp = d.toLocaleTimeString() + " on " + d.toLocaleDateString();
+		}
+	return resp;	
+}
